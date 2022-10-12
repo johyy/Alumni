8f307843bd7e351a88f812dfb7792657c880011a
Emil Calonius <emil.calonius@fi.experis.com>
2022-10-12 14:26:35 +0300

    feat: header dropdown menu displays current page

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,10 +7,25 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  dropDownText = "Timeline";
+  showTimelineOption = false;
+  showGroupsOption = true;
 
   constructor(public auth: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  handleClickTimelineButton() {
+    this.dropDownText = "Timeline";
+    this.showTimelineOption = false;
+    this.showGroupsOption = true;
+  }
+
+  handleClickGroupsButton() {
+    this.dropDownText = "Groups";
+    this.showGroupsOption = false;
+    this.showTimelineOption = true;
   }
 
 }
