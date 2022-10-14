2b6cac2b4ba2513674ad3f34952a465f20b3450c
Emil Calonius <emil.calonius@tuni.fi>
2022-10-14 11:56:10 +0300

    feat: fetch posts from backend and display on timeline

--- src/app/pages/timeline/timeline.page.ts
+++ src/app/pages/timeline/timeline.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post.model';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-timeline',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelinePage implements OnInit {
 
-  constructor() { }
+  get posts(): Post[] {
+    return this.postService.posts;
+  }
+
+  get loading(): boolean {
+    return this.postService.loading;
+  }
+
+  get error(): string {
+    return this.postService.error;
+  }
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.postService.findPosts()
   }
 
 }
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Post } from '../models/post.model';
+import { environment } from 'src/environments/environment';
+import { finalize } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private _posts: Post[] = [];
+  private _error: string = "";
+  private _loading: boolean = false;
+
+  get posts(): Post[] {
+    console.log(typeof(this._posts))
+    return this._posts;
+  }
+
+  get error(): string {
+    return this._error;
+  }
+
+  get loading(): boolean {
+    return this._loading;
+  }
+
+  constructor(private readonly http: HttpClient) { }
+
+  public findPosts(): void {
+    this._loading = true;
+    this.http.get<Post[]>(environment.apiPosts)
+    .pipe(
+      finalize(() => {
+        this._loading = false;
+      })
+    )
+    .subscribe({
+      next: (posts: Post[]) => {
+        this._posts = posts;
+      },
+      error: (error: HttpErrorResponse) => {
+        this._error = error.message;
+      }
+    })
+  }
+}
