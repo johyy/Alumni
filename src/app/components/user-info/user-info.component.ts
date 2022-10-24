5164427e370a0f4e54a76300dc608aeedea96b2b
Laura Byman <laura.byman@fi.experis.com>
2022-10-24 15:49:01 +0300

    Fixed group and topic buttons, and added edit profile button to show only on current user profile

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/models/user.model';
 import { Router} from '@angular/router';
+import { StorageUtil } from 'src/app/utils/storage.util';
+import { StorageKeys } from 'src/app/enums/storage-keys.enum';
 
 @Component({
   selector: 'app-user-info',
@@ -10,10 +12,20 @@ import { Router} from '@angular/router';
 export class UserInfoComponent implements OnInit {
 
   @Input() user: User = Object();
+  currentUser: User = Object();
 
   constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.currentUser = StorageUtil.storageReadOne(StorageKeys.User)!;;
+  }
+
+  isCurrentUser(): Boolean {
+    if(this.user.id == this.currentUser.id){
+      return true;
+    }else{
+      return false
+    }
   }
 
   navigateToEditUser() {
