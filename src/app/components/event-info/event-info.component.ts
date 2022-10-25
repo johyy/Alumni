08cfa0f91f17af3cbb0c34f101e41b1f81732afc
Laura Byman <laura.byman@fi.experis.com>
2022-10-25 13:17:31 +0300

    add comments to an event

--- src/app/components/event-info/event-info.component.ts
+++ src/app/components/event-info/event-info.component.ts
@@ -5,6 +5,7 @@ import { User } from 'src/app/models/user.model';
 import { PostService } from 'src/app/services/post.service';
 import { UserService } from 'src/app/services/user.service';
 import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-info',
@@ -15,11 +16,12 @@ export class EventInfoComponent implements OnInit, OnChanges {
   public posts?: Post[];
   public users?: User[];
   private refresh: Boolean = false;
+  private id = this.route.snapshot.paramMap.get('id')
 
   @Input() singleEvent : Event | undefined;
   @Input() eventHost: User | undefined;
 
-  constructor(private location: Location, private postService: PostService, private userService: UserService) { }
+  constructor(private location: Location, private postService: PostService, private userService: UserService, private router:Router, private route: ActivatedRoute) { }
   ngOnChanges(changes: SimpleChanges): void {
     if(changes['singleEvent']) this.refresh = !this.refresh;
   }
@@ -37,6 +39,13 @@ export class EventInfoComponent implements OnInit, OnChanges {
       }    
     )}
   }
+
+  
+  newPost():void{
+    this.router.navigate(['post','event', this.id]) 
+  }
+
+
 
   cancel(): void {
     this.location.back()
